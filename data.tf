0c84fa9a6fb3d681889f3944112a1062ef3052d2
PAblo <pablopaim65@gmail.com>
2024-10-24 21:35:49 -0300

    roteamento

--- data.tf
+++ data.tf
@@ -1,3 +1,7 @@
 data "aws_alb" "main" {
   arn = var.alb_arn
 }
+
+data "aws_ssm_parameter" "vpc" {
+  name = "/linuxtips-vpc/vpc/vpc_id"  # Certifique-se de que o nome do parâmetro esteja correto
+}
