45887f21363e808e26b40a436b556c283fd2d506
PAblo <pablopaim65@gmail.com>
2024-11-05 23:40:10 -0300

    1.4.1

--- data.tf
+++ data.tf
@@ -1,3 +1,4 @@
-data "aws_alb" "main" {
-  arn = var.alb_arn
+ data "aws_alb" "main" {
+  count = var.use_lb ? 1 : 0
+  arn   = var.alb_arn
 }
--- service.tf
+++ service.tf
@@ -68,10 +68,13 @@ resource "aws_ecs_service" "main" {
     assign_public_ip = false
   }
 
-  load_balancer {
-    target_group_arn = aws_alb_target_group.main.arn
-    container_name   = var.service_name
-    container_port   = var.service_port
+  dynamic "load_balancer" {
+    for_each = var.use_lb ? [1] : []
+    content {
+      target_group_arn = aws_alb_target_group.main[0].arn
+      container_name   = var.service_name
+      container_port   = var.service_port
+    }
   }
 
   lifecycle {
